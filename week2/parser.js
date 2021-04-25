bf53b729dcb0c2da1f9e493b6fc082f41e3e69f4
王伟敏 <wangwm@weipaitang.com>
2021-04-25 23:33:58 +0800

    feat：第二周第六节

--- week2/parser.js
+++ week2/parser.js
@@ -1,9 +1,51 @@
 const EOF = Symbol('EOF'); // EOF: End Of File
 
 let currentToken = null;
+let currentAttribute = null;
+let stack = [{type: 'document', children:[]}];
 
 function emit(token) {
     console.log(token);
+    if (token.type != 'text') {
+        return;
+    }
+
+    let top = stack[stack.length - 1];
+
+    if (token.type == 'startTag') {
+        let element = {
+            type: 'element',
+            children: [],
+            attributes: []
+        };
+
+        element.tagName = token.tagName;
+
+        for (let p in token) {
+            if (p != 'type' && p != 'tagName') {
+                element.attributes.push({
+                    name: p,
+                    value: token[p]
+                });
+            }
+        }
+
+        top.children.push(element);
+        element.parent = top;
+
+        if (!token.isSelfClosing) {
+            stack.push(element);
+        }
+
+        currentTextNode = null;
+    } else if (token.type == 'endTag') {
+        if (top.tagName != token.tagName) {
+            throw new Error('Tag start end doesn\'t match !');
+        } else {
+            stack.pop();
+        }
+        currentTextNode = null;
+    }
 }
 
 function data(c) {
@@ -33,6 +75,10 @@ function tagOpen(c) {
         }
         return tagName(c);
     } else {
+        emit({
+            type: 'text',
+            content: c
+        });
         return;
     }
 }
@@ -65,6 +111,7 @@ function tagName(c) {
         emit(currentToken);
         return data;
     } else {
+        currentToken.tagName += c;
         return tagName;
     }
 }
@@ -72,12 +119,114 @@ function tagName(c) {
 function beforeAttributeName(c) {
     if (c.match(/^[\t\n\f ]$/)) {
         return beforeAttributeName;
-    } else if (c == '>') {
-        return data;
+    } else if (c == '/' || c == '>' || c == EOF) {
+        return afterAttributeName(c);
     } else if (c == '=') {
-        return beforeAttributeName;
+        // return beforeAttributeName;
     } else {
+        // return beforeAttributeName;
+        currentAttribute = {
+            name: '',
+            value: ''
+        }
+        return attributeName(c);
+    }
+}
+
+function attributeName(c) {
+    if (c.match(/^[\t\n\f ]$/) || c == '/' || c == '>' || c == EOF) {
+        return afterAttributeName(c);
+    } else if (c == '=') {
+        return beforeAttributeValue;
+    } else if (c == '\u0000') {
+
+    } else if (c == '"' || c == '\'' || c == '<') {
+
+    } else {
+        currentAttribute.name += c;
+        return attributeName;
+    }
+}
+
+function beforeAttributeValue(c) {
+    if (c.match(/^[\t\n\f ]$/) || c == '/' || c == '>' || c == EOF) {
+        return beforeAttributeValue;
+    } else if (c == '"') {
+        return doubleQuotedAttributeValue;
+    } else if (c == '\'') {
+        return singleQuotedAttributeValue;
+    } else if (c == '>') {
+
+    } else {
+        return UnquotedAttributeValue(c);
+    }
+}
+
+function doubleQuotedAttributeValue(c) {
+    if (c == '"') {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return afterQuotedAttributeValue;
+    } else if (c == '\u0000') {
+
+    } else if (c == EOF) {
+
+    } else {
+        currentAttribute.value += c;
+        return doubleQuotedAttributeValue;
+    }
+}
+
+function singleQuotedAttributeValue(c) {
+    if (c == '\'') {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return afterQuotedAttributeValue;
+    } else if (c == '\u0000') {
+
+    } else if (c == EOF) {
+
+    } else {
+        currentAttribute.value += c;
+        return doubleQuotedAttributeValue;
+    }
+}
+
+function afterQuotedAttributeValue(c) {
+    if (c.match(/^[\t\n\f ]$/)) {
         return beforeAttributeName;
+    } else if (c == '/') {
+        return selfClosingStartTag;
+    } else if (c == '>') {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        emit(currentToken);
+        return data;
+    } else if (c == EOF) {
+
+    } else {
+        currentAttribute.value += c;
+        return doubleQuotedAttributeValue;
+    }
+}
+
+function UnquotedAttributeValue(c) {
+    if (c.match(/^[\t\nn\f ]$/)) {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return beforeAttributeName;
+    } else if (c == '/') {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return selfClosingStartTag;
+    } else if (c == '>') {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        emit(currentToken);
+        return data;
+    } else if (c =='\u0000') {
+
+    } else if (c == '"' || c == '\'' || c == '<' || c == '=' || c == '`') {
+
+    } else if (c == EOF) {
+
+    } else {
+        currentAttribute.value += c;
+        return UnquotedAttributeValue;
     }
 }
 
@@ -89,6 +238,29 @@ function selfClosingStartTag(c) {
 
     } else {
 
+    }
+}
+
+function  afterAttributeName(c) {
+    if (c.match(/^[\t\n\f ]$/)) {
+        return afterAttributeName;
+    } else if (c == '/') {
+        return selfClosingStartTag;
+    } else if (c == '=') {
+        return beforeAttributeValue;
+    } else if (c == '>') {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        emit(currentToken);
+        return data;
+    } else if (c == EOF) {
+
+    } else {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        currentAttribute = {
+            name: '',
+            value: ''
+        };
+        return attributeName(c);
     }
 }
 
